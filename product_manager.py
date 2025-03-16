0890934cf66642940e61c15e8a63d00b327ecbc3
Bozidar <bozidar.jankovic@hotmail.com>
2025-03-16 16:57:46 +0100

    Dodavanje metode za uklanjanje proizvoda

--- product_manager.py
+++ product_manager.py
@@ -13,3 +13,6 @@ class ProductManager:
             
     def total_inventory_value(self):
         return sum(product.price * product.quantity for product in self.products)
+    
+    def remove_product(self, name):
+    self.products = [product for product in self.products if product.name != name]
